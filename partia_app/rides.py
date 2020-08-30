821547d4579d75f3c238baade1392fc1a30c7ca4
OrShay <62897690+OrShay@users.noreply.github.com>
2020-08-30 19:19:04 +0300

    Add "Get Rides" API call

--- partia_app/rides.py
+++ partia_app/rides.py
@@ -49,22 +49,3 @@ def get_rides(pin_code):
         return responses.response_internal_error(e)
     return responses.response_200(json_response)
 
-
-@rides_blueprint.route('/rides/set-data', methods=['POST'])
-def add_ride():
-    errors = validator.RideScheme().validate(request.json)
-    if errors:
-        return responses.response_invalid_request(errors)
-    event = AppEngine.get_event_by_pin_code(request.json["pin_code"])
-    if not event:
-        return responses.response_invalid_event()
-    try:
-        driver_user_name = request.json['driver_user_name']
-        driver = event.participants_dict[driver_user_name]
-        source = request.json['source']
-        available_seats = request.json['available_seats']
-        departure_time = request.json['departure_time']
-        event.rides_board.add_ride(driver, available_seats, source, departure_time)
-        return responses.response_200({"response": "added ride"})
-    except KeyError:
-        return responses.response_invalid_user_name()
