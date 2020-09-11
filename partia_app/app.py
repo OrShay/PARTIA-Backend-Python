a8190089a8318fd4053b2c5ca01109a6cd62bc1b
OrShay <62897690+OrShay@users.noreply.github.com>
2020-09-11 13:40:44 +0300

    Add is alive method

--- partia_app/app.py
+++ partia_app/app.py
@@ -4,10 +4,9 @@ from partia_app.equipment import equipment_blueprint
 from partia_app.participant import participant_blueprint
 from partia_app.login import login_blueprint
 from flask import Flask, request
-
+from partia_app.responses import response_200
 
 app = Flask(__name__)
-
 
 app.register_blueprint(rides_blueprint)
 app.register_blueprint(event_blueprint)
@@ -16,6 +15,10 @@ app.register_blueprint(participant_blueprint)
 app.register_blueprint(login_blueprint)
 
 
+@app.route('/', methods=['GET'])
+def is_alive():
+    return response_200({"message": "The server is up Queen! :)"})
+
+
 if __name__ == '__main__':
     app.run()
-
