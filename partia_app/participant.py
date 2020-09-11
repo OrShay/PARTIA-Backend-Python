19332507f59d30ff0ea2deabaf92586e5eee7c73
OrShay <62897690+OrShay@users.noreply.github.com>
2020-09-11 16:51:17 +0300

    Add logger

--- partia_app/participant.py
+++ partia_app/participant.py
@@ -6,7 +6,7 @@ from flask_request_validator import (
     GET,
     validate_params
 )
-
+from partia_app.app import app
 participant_blueprint = Blueprint('participant_blueprint', __name__)
 
 
@@ -53,14 +53,13 @@ def is_participant_event_owner(pin_code, user_name):
 
 @participant_blueprint.route('/participant/events', methods=['POST'])
 def get_participant_events():
-    try:
-        user_email = request.json.get('userEmail', None)
-        if not user_email:
-            return responses.response_invalid_request({"message": "UserEmil is required"})
-        if user_email not in AppEngine.users_dict.keys():
-            return responses.response_invalid_user_name()
-        events_dict = AppEngine.get_user_events(user_email)
-        return responses.response_200(events_dict)
-    except Exception as ex:
-        print("Error!!")
-        return responses.response_internal_error(ex)
+    app.logger.debug("In get_participant_events")
+    app.logger.debug(f"Got request: {request} of type: {type(request)}")
+    app.logger.debug(f"Got json: {request.json} ")
+    user_email = request.json.get('userEmail', None)
+    if not user_email:
+        return responses.response_invalid_request({"message": "UserEmil is required"})
+    if user_email not in AppEngine.users_dict.keys():
+        return responses.response_invalid_user_name()
+    events_dict = AppEngine.get_user_events(user_email)
+    return responses.response_200(events_dict)
